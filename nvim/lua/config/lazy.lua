7f5eb1f2011b0d3a607b13fff9d6db72897e1835
aiirononeko <aiirononeko@gmail.com>
2024-11-12 15:37:07 +0900

    :beers: Enable project-specific configuration for biome, prettier, and eslint

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,15 +14,60 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local function biome_lsp_or_prettier(bufnr)
+  local has_biome_lsp = vim.lsp.get_clients({
+    bufnr = bufnr,
+    name = "biome",
+  })[1]
+  if has_biome_lsp then
+    return {}
+  end
+  local has_prettier = vim.fs.find({
+    -- https://prettier.io/docs/en/configuration.html
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    ".prettierrc.toml",
+    "prettier.config.js",
+    "prettier.config.cjs",
+  }, { upward = true })[1]
+  if has_prettier then
+    return { "prettier" }
+  end
+  return { "biome" }
+end
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "solarized-osaka",
-    } },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "solarized-osaka",
+      },
+    },
     { import = "lazyvim.plugins.extras.lang.prisma" },
     -- import/override with your plugins
     { import = "plugins" },
+    {
+      "stevearc/conform.nvim",
+      ---@class ConformOpts
+      opts = {
+        formatters_by_ft = {
+          javascript = biome_lsp_or_prettier,
+          typescript = biome_lsp_or_prettier,
+          javascriptreact = biome_lsp_or_prettier,
+          typescriptreact = biome_lsp_or_prettier,
+          json = { "biome" },
+          jsonc = { "biome" },
+        },
+      },
+    },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
