74ebf177515d111360017b77cb1f0a0d694b1a40
aiirononeko <aiirononeko@gmail.com>
2026-03-06 03:11:28 +0900

    feat: ツールチップを導入

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -14,6 +14,45 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle file tree" },
+    },
+    opts = {
+      close_if_last_window = true,
+      filesystem = {
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      window = {
+        width = 30,
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true,
+        },
+      },
+    },
+  },
+  {
     "echasnovski/mini.pick",
     version = false,
     keys = {
